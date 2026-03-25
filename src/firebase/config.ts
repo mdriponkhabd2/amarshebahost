25e8249c9930969a62c5697bafe91ac0100449f2
Md Ripon Kha <mdriponkhabd@gmail.com>
2026-03-25 09:35:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7605373653-e8593",
+  "appId": "1:688525667372:web:5c571685190cc0f35df589",
+  "apiKey": "AIzaSyDODjAvDTee77XNHpBPzNTXM8_JVNQGfDY",
+  "authDomain": "studio-7605373653-e8593.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "688525667372"
+};
